c3788980142212f0f7ae3f5358596fdc97f177dd
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-04-01 22:43:00 -0300

    feat: define basic vue project

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+  <header class="container">
+    <img src="../assets/logo.png" alt="Altave" />
+  </header>
+</template>
+
+<style scoped>
+.container {
+  box-sizing: border-box;
+  padding: 8px 16px;
+  background-color: var(--gray);
+}
+
+.container img {
+  height: 52px;
+  width: auto;
+}
+</style>
