5b7033a99e0a304a10014334ab5bfce83951b071
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-04-03 07:41:03 -0300

    feat: basic download report interface

---
+++ src/components/Botao.vue
@@ -0,0 +1,70 @@
+<script lang="ts">
+import type { PropType } from "vue";
+
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String as PropType<"primary" | "secondary">,
+      default: "primary"
+    },
+    size: {
+      type: String as PropType<"normal" | "large">,
+      default: "normal"
+    },
+  }
+}
+</script>
+
+<template>
+  <button 
+    :class="[
+      'btn', 
+      {
+        primary: color == 'primary', 
+        secondary: color == 'secondary'
+      },
+      {
+        large: size == 'large'
+      }
+    ]"
+  >
+    {{ content }}
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  font-weight: bold;
+  color: var(--white);
+  padding: 8px 12px;
+  border-radius: 12px;
+  cursor: pointer;
+  transition: all 0.25s;
+}
+
+.primary {
+  background-color: var(--dark-blue);
+  min-width: 120px;
+}
+
+.primary:hover {
+  background-color: var(--blue);
+}
+
+.secondary {
+  background-color: var(--light-gray);
+  min-width: 120px;
+}
+
+.secondary:hover {
+  background-color: var(--light-gray-100);
+}
+
+.large {
+  padding: 18px 12px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,37 @@
 <script setup lang="ts">
+import Titulo from '@/components/Titulo.vue';
+import Botao from '@/components/Botao.vue';
+
+const submit = () => {
+  console.log('submit')
+}
 </script>
 
 <template>
-  <main></main>
+  <main class='main'>
+    <Titulo content="Relatórios" />
+    <div class="fields-container">
+
+    </div>
+    <div class="btn-container">
+      <Botao @click="submit" class="btn" content="Download Relatório" />
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  gap: 24px
+}
+
+.btn-container {
+  display: flex;
+  justify-content: center;
+}
+
+.btn {
+  padding-inline: 52px;
+}
+</style>
