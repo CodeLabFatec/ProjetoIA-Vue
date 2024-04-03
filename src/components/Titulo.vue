e1ea0c19a5d7e153df85a7ec25807c98063d0186
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-04-03 07:41:03 -0300

    feat: basic download report interface

---
+++ src/components/Titulo.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <h1 class="titulo">
+    {{ content }}
+  </h1>
+</template>
+
+<style scoped>
+.titulo {
+  text-align: center;
+  padding: 18px 64px;
+  border-bottom: 2px dotted black;
+  margin: 32px auto;
+  width: fit-content;
+}
+</style>
