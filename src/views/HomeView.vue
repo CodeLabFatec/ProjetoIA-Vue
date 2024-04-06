33c02cfa1f39425fffcd63a7a07948db8ba4a763
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-04-06 19:46:01 -0300

    feat: select date input on download report interface

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,41 @@
 <script setup lang="ts">
+import type { ISelectedDate } from '@/interfaces/ISelectedDate';
+
+import { ref } from 'vue';
+
 import Titulo from '@/components/Titulo.vue';
 import Botao from '@/components/Botao.vue';
+import SeletorData from '@/components/SeletorData.vue';
+
+const selectedDate = ref<ISelectedDate>(null);
 
 const submit = () => {
   console.log('submit')
 }
+
+const onChange = (date: ISelectedDate) => {
+  selectedDate.value = date;
+}
+
+
+const reset = () => {
+  selectedDate.value = null;
+}
+
 </script>
 
 <template>
   <main class='main'>
     <Titulo content="Relatórios" />
     <div class="fields-container">
-
+      <SeletorData 
+        label="Selecione uma data"
+        :value="selectedDate"
+        @on-change="onChange($event)"
+      />
     </div>
     <div class="btn-container">
-      <Botao @click="submit" class="btn" content="Download Relatório" />
+      <Botao @click="submit()" class="btn" content="Download Relatório" />
     </div>
   </main>
 </template>
@@ -24,6 +45,11 @@ const submit = () => {
   display: flex;
   flex-direction: column;
   gap: 24px
+}
+
+.fields-container {
+  display: flex;
+  justify-content: center;
 }
 
 .btn-container {
