62ec22d9fc5832f16e4a0a8aee77d117bfdcbccc
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-04-13 14:34:15 -0300

    fix: change api base url to right url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:5007",
   headers: {
     "Content-Type": "application/json",
   },
