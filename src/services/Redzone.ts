1d58e45b945f22622a24da5ad58b42658a3101d7
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-04-17 21:41:11 -0300

    feat: redzones list api request

--- src/services/Redzone.ts
+++ src/services/Redzone.ts
@@ -1,37 +1,16 @@
 import type IRedzone from "@/interfaces/IRedzone";
+import api from "./api";
 
 class Redzone {
   async getRedzones(): Promise<{ data: IRedzone[]; status: number }> {
-    const status = 200;
+    try {
+      const { data, status } = await api.get("/redzone");
 
-    const list: IRedzone[] = [
-      {
-        data_cadastro: new Date(2002).toUTCString(),
-        descricao: "teste 1",
-        id: 1,
-        nome: "teste 1",
-      },
-      {
-        data_cadastro: new Date().toUTCString(),
-        descricao: "teste",
-        id: 2,
-        nome: "teste 2",
-      },
-      {
-        data_cadastro: new Date().toUTCString(),
-        descricao: "teste",
-        id: 3,
-        nome: "teste 3",
-      },
-      {
-        data_cadastro: new Date().toUTCString(),
-        descricao: "teste",
-        id: 4,
-        nome: "teste 4",
-      },
-    ];
-
-    return { data: list, status };
+      return { data: status == 200 ? data : [], status };
+    } catch (err) {
+      console.log(err);
+      return { data: [], status: 500 };
+    }
   }
 }
 
