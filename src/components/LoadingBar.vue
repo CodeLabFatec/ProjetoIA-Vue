afea204eb53d241454e1a9955fd575d85bded0a1
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-04-30 10:45:50 -0300

    feat: create component loading bar

---
+++ src/components/LoadingBar.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="loading-bar">
+    <v-progress-linear v-if="visible" indeterminate color="#004488"></v-progress-linear>
+  </div>
+</template>
+
+<style scoped>
+.loading-bar {
+  position: absolute;
+  width: 100%;
+  top: 74px;
+  left: 0;
+}
+</style>
