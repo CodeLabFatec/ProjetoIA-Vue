2912118254a97c70335549541803444e8b255796
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-05-04 20:40:23 -0300

    fix: change api python baseurl

--- src/services/apiPython.ts
+++ src/services/apiPython.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosInstance } from "axios";
 
 const apiPython: AxiosInstance = axios.create({
-  baseURL: "http://192.168.15.89:5007",
+  baseURL: "http://localhost:5007",
   headers: {
     "Content-Type": "application/json",
   },
