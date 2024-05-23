b6736c135bc1a60c7863821c909327022ee791cf
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-05-23 10:03:42 -0300

    build: config apis base url on env

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_SPRING_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
--- src/services/apiPython.ts
+++ src/services/apiPython.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosInstance } from "axios";
 
 const apiPython: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5007",
+  baseURL: import.meta.env.VITE_API_PYTHON_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
